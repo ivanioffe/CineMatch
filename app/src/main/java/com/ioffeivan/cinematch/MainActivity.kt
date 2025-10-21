38088ff94d437df474d5f12ab2e6c7875abba092
Ivan Ioffe <iiooffee0202@gmail.com>
2025-10-21 23:15:00 +0300

    Initial commit

---
+++ app/src/main/java/com/ioffeivan/cinematch/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ioffeivan.cinematch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.ioffeivan.cinematch.ui.theme.CineMatchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            CineMatchTheme {
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/ioffeivan/cinematch/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.ioffeivan.cinematch.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography()
