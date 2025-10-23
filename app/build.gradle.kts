243451e83a5d74f88921bfb2d6ecdf0f219c74b4
Ivan Ioffe <iiooffee0202@gmail.com>
2025-10-23 17:23:58 +0300

    feat(designsystem): add Material3 theme
    - Added Color.kt, Type.kt, Theme.kt with dark color scheme
    - Removed theme files from app module
    - Connected core:designsystem to app
    - Updated app/themes.xml (status/navigation bars colors)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ android {
 
 dependencies {
 
+    implementation(projects.core.designsystem)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
