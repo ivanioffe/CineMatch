a92315b93e2ec35bd78f63169de7ab5ebe2bc054
Ivan Ioffe <iiooffee0202@gmail.com>
2025-10-31 17:03:03 +0300

    build(network): Inject BACKEND_BASE_URL from local.properties into BuildConfig in :core:network module

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,9 +1,21 @@
+import java.io.StringReader
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.kotlin.serialization)
 }
+
+val backendBaseUrl =
+    providers.fileContents(
+        isolated.rootProject.projectDirectory.file("local.properties"),
+    ).asText.map { text ->
+        val properties = Properties()
+        properties.load(StringReader(text))
+        properties["BACKEND_BASE_URL"] as String
+    }.orElse("")
 
 android {
     namespace = "com.ioffeivan.core.network"
@@ -14,6 +26,8 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+
+        buildConfigField("String", "BACKEND_BASE_URL", "\"${backendBaseUrl.get()}\"")
     }
 
     buildTypes {
@@ -31,6 +45,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+    }
+    buildFeatures {
+        buildConfig = true
     }
 }
 
