87ff75d4045120b7ab1ed10b044e262e7368ac77
Ivan Ioffe <iiooffee0202@gmail.com>
2025-11-16 23:28:26 +0300

    feat(ui): add LoadingScreen composable

---
+++ core/ui/src/main/java/com/ioffeivan/core/ui/LoadingScreen.kt
@@ -0,0 +1,25 @@
+package com.ioffeivan.core.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun LoadingScreen(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier =
+            modifier
+                .background(Color.Black.copy(0.5f))
+                .clickable(enabled = false) {},
+    ) {
+        CircularProgressIndicator()
+    }
+}
