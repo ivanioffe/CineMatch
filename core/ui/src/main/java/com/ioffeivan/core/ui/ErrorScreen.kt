9cd6e369c1163bc34173c9ab60adb12aeae22041
Ivan Ioffe <iiooffee0202@gmail.com>
2025-12-20 22:27:29 +0300

    feat(ui): add ErrorScreen

---
+++ core/ui/src/main/java/com/ioffeivan/core/ui/ErrorScreen.kt
@@ -0,0 +1,41 @@
+package com.ioffeivan.core.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ErrorScreen(
+    errorMessage: String,
+    onRetryClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier,
+    ) {
+        Text(
+            text = errorMessage,
+            style =
+                MaterialTheme.typography.bodyLarge
+                    .copy(
+                        textAlign = TextAlign.Center,
+                    ),
+        )
+
+        Button(
+            onClick = onDebounceClick(onClick = onRetryClick),
+        ) {
+            Text(text = stringResource(R.string.retry))
+        }
+    }
+}
