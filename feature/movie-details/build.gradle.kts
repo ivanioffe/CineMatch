8523336de90a238b5cb750104551e6861fd6217e
Ivan Ioffe <iiooffee0202@gmail.com>
2025-12-23 22:31:49 +0300

    feat(movie-details): implement MovieDetailsScreen and navigation
    - Added MovieDetailsScreen composable
    - Setup movie details navigation
    - Added screenshot tests for content, error, and loading states
    - Updated build.gradle.kts with necessary dependencies

--- feature/movie-details/build.gradle.kts
+++ feature/movie-details/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.roborazzi)
 }
 
 android {
@@ -34,21 +36,43 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
 }
 
 dependencies {
     implementation(projects.core.common)
+    implementation(projects.core.designsystem)
     implementation(projects.core.network)
     implementation(projects.core.presentation)
+    implementation(projects.core.ui)
 
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
     implementation(libs.hilt.android)
 
     ksp(libs.hilt.compiler)
 
+    testImplementation(libs.androidx.ui.test.junit4)
+    testImplementation(libs.androidx.ui.test.manifest)
     testImplementation(libs.junit.vintage.engine)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mockk)
     testImplementation(libs.mockwebserver)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.roborazzi)
+    testImplementation(libs.roborazzi.compose)
+    testImplementation(libs.roborazzi.rule)
     testImplementation(platform(libs.test.junit5.bom))
     testImplementation(libs.test.junit5.api)
     testImplementation(libs.test.junit5.params)
@@ -60,4 +84,6 @@ dependencies {
     testRuntimeOnly(libs.test.junit5.engine)
 
     androidTestImplementation(libs.androidx.test.runner)
+
+    debugImplementation(libs.androidx.ui.tooling)
 }
