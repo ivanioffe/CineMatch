1b68bf2e7a210782644d43f914981de0cfedea9a
Ivan Ioffe <iiooffee0202@gmail.com>
2026-01-02 23:24:12 +0300

    build: add :build-logic:convention module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.ioffeivan.cinematch.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
