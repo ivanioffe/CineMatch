ddc2e8e2f4c87e9ecf6a5cdd6d64eab4df7edc74
Ivan Ioffe <iiooffee0202@gmail.com>
2026-01-03 22:53:50 +0300

    build: migrate all modules to convention plugins
    - Replace redundant gradle configurations with internal convention plugins
    - Remove unused dependencies in modules
    - Remove tests from app module

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,18 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.cinematch.android.library)
+    alias(libs.plugins.cinematch.compose)
     alias(libs.plugins.ktlint)
 }
 
 android {
     namespace = "com.ioffeivan.core.designsystem"
-    compileSdk = 36
 
     defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -25,22 +20,4 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-
-    debugImplementation(libs.androidx.ui.tooling)
 }
