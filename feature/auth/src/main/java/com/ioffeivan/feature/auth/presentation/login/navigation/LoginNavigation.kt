be2e2f9aa019ee83bf39504fa47798fb6f602a13
Ivan Ioffe <iiooffee0202@gmail.com>
2026-02-06 14:03:54 +0300

    refactor(auth-navigation): introduce nested graphs for login and signup flows
    - Wrap Login in a nested navigation graph for consistency and future extensibility.
    - Group SignUp, EmailVerification, and AccountCreated into a single nested graph to manage multi-step registration flow.
    - Make internal routes and builders private to encapsulate subgraph details.
    - Add onNavigateToAccountCreated callback to SignUp builder for proper navigation chaining.

--- feature/auth/src/main/java/com/ioffeivan/feature/auth/presentation/login/navigation/LoginNavigation.kt
+++ feature/auth/src/main/java/com/ioffeivan/feature/auth/presentation/login/navigation/LoginNavigation.kt
@@ -4,26 +4,34 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import com.ioffeivan.core.ui.ShowSnackbar
 import com.ioffeivan.feature.auth.presentation.login.composable.LoginRoute
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object LoginRoute
+data object LoginBaseRoute
+
+@Serializable
+internal data object LoginRoute
 
 fun NavHostController.navigateToLogin(navOptions: NavOptions? = null) =
-    navigate(LoginRoute, navOptions)
+    navigate(LoginBaseRoute, navOptions)
 
 fun NavGraphBuilder.login(
     onNavigateToSignUp: () -> Unit,
     onNavigateToMain: () -> Unit,
     onShowSnackbar: ShowSnackbar,
 ) {
-    composable<LoginRoute> {
-        LoginRoute(
-            onNavigateToSignUp = onNavigateToSignUp,
-            onNavigateToMain = onNavigateToMain,
-            onShowSnackbar = onShowSnackbar,
-        )
+    navigation<LoginBaseRoute>(
+        startDestination = LoginRoute,
+    ) {
+        composable<LoginRoute> {
+            LoginRoute(
+                onNavigateToSignUp = onNavigateToSignUp,
+                onNavigateToMain = onNavigateToMain,
+                onShowSnackbar = onShowSnackbar,
+            )
+        }
     }
 }
