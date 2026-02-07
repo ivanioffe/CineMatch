c6ed5ada47fc3e3cd3c12009e0849319e3256257
Ivan Ioffe <iiooffee0202@gmail.com>
2026-02-07 15:11:05 +0300

    feat(app): implement edge-to-edge support and window insets handling

--- app/src/main/java/com/ioffeivan/cinematch/MainActivity.kt
+++ app/src/main/java/com/ioffeivan/cinematch/MainActivity.kt
@@ -3,9 +3,14 @@ package com.ioffeivan.cinematch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ioffeivan.cinematch.ui.AppScreen
 import com.ioffeivan.core.designsystem.theme.CineMatchTheme
@@ -18,9 +23,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         setContent {
             CineMatchTheme {
-                Surface {
+                Surface(
+                    modifier =
+                        Modifier
+                            .windowInsetsPadding(WindowInsets.safeDrawing),
+                ) {
                     val uiState by mainActivityViewModel.uiState.collectAsStateWithLifecycle()
 
                     if (uiState !is MainActivityUiState.Loading) {
